60f085a6e607d18330c30dafde21d03911cdd411
PaulJonatha <p.ramirez9991@gmail.com>
2022-02-03 15:19:00 -0300

    Tarea echa

--- 1-EntradaSalida/jsEntradaSalida-02.js
+++ 1-EntradaSalida/jsEntradaSalida-02.js
@@ -1,7 +1,7 @@
 /*
 Alumno :Paul Ramirez Benites
 Ejercicio 2
-
+Enviado 03/02/22
 Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'mostrar'*/
 function mostrar()
--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,7 +1,7 @@
 /*
 Alumno :Paul Ramirez Benites
 Ejercicio 3
-
+Enviado
 
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/

5d49f58d0ebb5cdbf23c6549591d89281c96a52b
PaulJonatha <p.ramirez9991@gmail.com>
2022-02-03 00:14:41 -0300

    Trabajo 3 y 4 echos

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -4,8 +4,11 @@ y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	var nombreIngresado;
+
 	nombreIngresado=txtIdNombre.value;
 	alert(nombreIngresado);
+	txtIdNombre.value = ""; //Para sacar el nombre ingresado al terminar.
+	
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -4,6 +4,15 @@ y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	//reservo espacio a la memoria para guardar un dato
+	let Dato;
+
+    //guardo en la variable el dato en el texto que escribio el usario en prom
+     Dato = prompt("Ingrese un dato");
+     
+    // copio el dato que tengo guardo en la variable dato dentro en la caja de texto
+     txtIdNombre.value = Dato;
+
+	
 }
 
